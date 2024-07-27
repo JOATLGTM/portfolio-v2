8448fcb639575292f81ea457af408cd8e9684ec4
JOATLGTM <66840886+JOATLGTM@users.noreply.github.com>
2024-07-27 01:02:57 -0500

    update navbar tabs

--- data/index.ts
+++ data/index.ts
@@ -1,7 +1,7 @@
 export const navItems = [
     { name: "About", link: "#about" },
     { name: "Projects", link: "#projects" },
-    { name: "Testimonials", link: "#testimonials" },
+    { name: "Work Experience", link: "#experience" },
     { name: "Contact", link: "#contact" },
 ];
 
@@ -49,8 +49,8 @@ export const gridItems = [
 
     {
         id: 5,
-        title: "Currently building a JS Animation library",
-        description: "The Inside Scoop",
+        title: "Currently mastering more libraries and frameworks such as Next. Three.js and GSAP.",
+        description: "What am currently doing?",
         className: "md:col-span-3 md:row-span-2",
         imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
         titleClassName: "justify-center md:justify-start lg:justify-center",
