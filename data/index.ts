1998eccb544c58b9c1700a75a894fa855e78ba3c
Chris Vo <cv@Chriss-MacBook-Pro.local>
2025-02-15 16:04:01 -0600

    update projects. Added image to reflect of the new projects

--- data/index.ts
+++ data/index.ts
@@ -1,216 +1,203 @@
 export const navItems = [
-    { name: "Home", link: "#home" },
-    { name: "About", link: "#about" },
-    { name: "Projects", link: "#projects" },
-    { name: "Experience", link: "#experience" },
-    { name: "Contact", link: "#contact" },
+	{ name: "Home", link: "#home" },
+	{ name: "About", link: "#about" },
+	{ name: "Projects", link: "#projects" },
+	{ name: "Experience", link: "#experience" },
+	{ name: "Contact", link: "#contact" },
 ];
 
 export const gridItems = [
-    {
-        id: 1,
-        title: "I prioritize client collaboration, fostering open communication ",
-        description: "",
-        className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
-        imgClassName: "w-full h-full",
-        titleClassName: "justify-end",
-        img: "/b1.svg",
-        spareImg: "",
-    },
-    {
-        id: 2,
-        title: "I'm very flexible with time zone communications",
-        description: "",
-        className: "lg:col-span-2 md:col-span-3 md:row-span-2",
-        imgClassName: "",
-        titleClassName: "justify-start",
-        img: "",
-        spareImg: "",
-    },
-    {
-        id: 3,
-        title: "My tech stack",
-        description: "I constantly try to improve",
-        className: "lg:col-span-2 md:col-span-3 md:row-span-2",
-        imgClassName: "",
-        titleClassName: "justify-center",
-        img: "",
-        spareImg: "",
-    },
-    {
-        id: 4,
-        title: "Tech enthusiast with a passion for development.",
-        description: "",
-        className: "lg:col-span-2 md:col-span-3 md:row-span-1",
-        imgClassName: "",
-        titleClassName: "justify-start",
-        img: "/grid.svg",
-        spareImg: "/b4.svg",
-    },
+	{
+		id: 1,
+		title: "I prioritize client collaboration, fostering open communication ",
+		description: "",
+		className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+		imgClassName: "w-full h-full",
+		titleClassName: "justify-end",
+		img: "/b1.svg",
+		spareImg: "",
+	},
+	{
+		id: 2,
+		title: "I'm very flexible with time zone communications",
+		description: "",
+		className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+		imgClassName: "",
+		titleClassName: "justify-start",
+		img: "",
+		spareImg: "",
+	},
+	{
+		id: 3,
+		title: "My tech stack",
+		description: "I constantly try to improve",
+		className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+		imgClassName: "",
+		titleClassName: "justify-center",
+		img: "",
+		spareImg: "",
+	},
+	{
+		id: 4,
+		title: "Tech enthusiast with a passion for development.",
+		description: "",
+		className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+		imgClassName: "",
+		titleClassName: "justify-start",
+		img: "/grid.svg",
+		spareImg: "/b4.svg",
+	},
 
-    {
-        id: 5,
-        title: "Currently mastering more libraries and frameworks such as React.js, Next.js, Three.js and GSAP.",
-        description: "What am currently doing?",
-        className: "md:col-span-3 md:row-span-2",
-        imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
-        titleClassName: "justify-center md:justify-start lg:justify-center",
-        img: "/b5.svg",
-        spareImg: "/grid.svg",
-    },
-    {
-        id: 6,
-        title: "Do you want to start a project together?",
-        description: "",
-        className: "lg:col-span-2 md:col-span-3 md:row-span-1",
-        imgClassName: "",
-        titleClassName: "justify-center md:max-w-full max-w-60 text-center",
-        img: "",
-        spareImg: "",
-    },
+	{
+		id: 5,
+		title: "Currently mastering more libraries and frameworks such as React.js, Next.js, Three.js and GSAP.",
+		description: "What am currently doing?",
+		className: "md:col-span-3 md:row-span-2",
+		imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+		titleClassName: "justify-center md:justify-start lg:justify-center",
+		img: "/b5.svg",
+		spareImg: "/grid.svg",
+	},
+	{
+		id: 6,
+		title: "Do you want to start a project together?",
+		description: "",
+		className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+		imgClassName: "",
+		titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+		img: "",
+		spareImg: "",
+	},
 ];
 
 export const projects = [
-    {
-        id: 1,
-        title: "Remix Expense Project",
-        des: "An Expense list with CRUD abilities using Remix.js",
-        img: "/remix-project.png",
-        iconLists: ["/remix-run-line.svg"],
-        link: "https://remix-project-silk.vercel.app/",
-    },
-    {
-        id: 2,
-        title: "Ecommerce project selling products",
-        des: "Selling music electronics using Next.js, sanity.io and Stripe API",
-        img: "/ecommerce-stripe.png",
-        iconLists: ["/next.svg", "/stripe-svgrepo-com.svg", "/sanity-svgrepo-com.svg"],
-        link: "https://ecommerce-stripe-dun.vercel.app",
-    },
-    // {
-    //     id: 3,
-    //     title: "AI Image SaaS - Canva Application",
-    //     des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
-    //     img: "/p3.svg",
-    //     iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
-    //     link: "/ui.aiimg.com",
-    // },
-    // {
-    //     id: 4,
-    //     title: "Animated Apple Iphone 3D Website",
-    //     des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
-    //     img: "/p4.svg",
-    //     iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
-    //     link: "/ui.apple.com",
-    // },
+	{
+		id: 1,
+		title: "Mo Muscles",
+		des: "Get the best training for your physical needs",
+		img: "/gymImg.png",
+		iconLists: ["/next.svg"],
+		link: "https://www.trainmomuscle.com/",
+	},
+	{
+		id: 2,
+		title: "Remix Expense Project",
+		des: "Get your best nail services in Columbus Ohio",
+		img: "/nailsalon.png",
+		iconLists: ["/next.svg"],
+		link: "https://www.amelianailscolumbus.com/",
+	},
+	{
+		id: 3,
+		title: "Remix Expense Project",
+		des: "An Expense list with CRUD abilities using Remix.js",
+		img: "/remix-project.png",
+		iconLists: ["/remix-run-line.svg"],
+		link: "https://remix-project-silk.vercel.app/",
+	},
 ];
 
 export const testimonials = [
-    {
-        quote:
-            "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-        name: "Michael Johnson",
-        title: "Director of AlphaStream Technologies",
-    },
-    {
-        quote:
-            "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-        name: "Michael Johnson",
-        title: "Director of AlphaStream Technologies",
-    },
-    {
-        quote:
-            "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-        name: "Michael Johnson",
-        title: "Director of AlphaStream Technologies",
-    },
-    {
-        quote:
-            "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-        name: "Michael Johnson",
-        title: "Director of AlphaStream Technologies",
-    },
-    {
-        quote:
-            "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-        name: "Michael Johnson",
-        title: "Director of AlphaStream Technologies",
-    },
+	{
+		quote: "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+		name: "Michael Johnson",
+		title: "Director of AlphaStream Technologies",
+	},
+	{
+		quote: "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+		name: "Michael Johnson",
+		title: "Director of AlphaStream Technologies",
+	},
+	{
+		quote: "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+		name: "Michael Johnson",
+		title: "Director of AlphaStream Technologies",
+	},
+	{
+		quote: "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+		name: "Michael Johnson",
+		title: "Director of AlphaStream Technologies",
+	},
+	{
+		quote: "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+		name: "Michael Johnson",
+		title: "Director of AlphaStream Technologies",
+	},
 ];
 
 export const companies = [
-    {
-        id: 1,
-        name: "cloudinary",
-        img: "/cloud.svg",
-        nameImg: "/cloudName.svg",
-    },
-    {
-        id: 2,
-        name: "appwrite",
-        img: "/app.svg",
-        nameImg: "/appName.svg",
-    },
-    {
-        id: 3,
-        name: "HOSTINGER",
-        img: "/host.svg",
-        nameImg: "/hostName.svg",
-    },
-    {
-        id: 4,
-        name: "stream",
-        img: "/s.svg",
-        nameImg: "/streamName.svg",
-    },
-    {
-        id: 5,
-        name: "docker.",
-        img: "/dock.svg",
-        nameImg: "/dockerName.svg",
-    },
+	{
+		id: 1,
+		name: "cloudinary",
+		img: "/cloud.svg",
+		nameImg: "/cloudName.svg",
+	},
+	{
+		id: 2,
+		name: "appwrite",
+		img: "/app.svg",
+		nameImg: "/appName.svg",
+	},
+	{
+		id: 3,
+		name: "HOSTINGER",
+		img: "/host.svg",
+		nameImg: "/hostName.svg",
+	},
+	{
+		id: 4,
+		name: "stream",
+		img: "/s.svg",
+		nameImg: "/streamName.svg",
+	},
+	{
+		id: 5,
+		name: "docker.",
+		img: "/dock.svg",
+		nameImg: "/dockerName.svg",
+	},
 ];
 
 export const workExperience = [
-    {
-        id: 1,
-        title: "Software Engineer at Gap",
-        desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
-        className: "md:col-span-2",
-        thumbnail: "/gaplogo.png",
-    },
-    {
-        id: 2,
-        title: "React developer at Blended Edge",
-        desc: "Designed and developed an user interface in Shopify to seamlessly connect APIs.",
-        className: "md:col-span-2", // change to md:col-span-2
-        thumbnail: "/blendededge_logo.jpeg",
-    },
-    {
-        id: 3,
-        title: "Software Engineer at Beam Dental",
-        desc: "Developed single paged applications using React and Ruby on Rails. Go team Space Whales",
-        className: "md:col-span-2", // change to md:col-span-2
-        thumbnail: "/beambenefits_logo.jpeg",
-    },
-    {
-        id: 4,
-        title: "Frontend Developer at Wired Campaigns",
-        desc: "Developed and maintained user-facing features using modern frontend technologies.",
-        className: "md:col-span-2",
-        thumbnail: "/exp4.svg",
-    },
+	{
+		id: 1,
+		title: "Software Engineer at Gap",
+		desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+		className: "md:col-span-2",
+		thumbnail: "/gaplogo.png",
+	},
+	{
+		id: 2,
+		title: "React developer at Blended Edge",
+		desc: "Designed and developed an user interface in Shopify to seamlessly connect APIs.",
+		className: "md:col-span-2", // change to md:col-span-2
+		thumbnail: "/blendededge_logo.jpeg",
+	},
+	{
+		id: 3,
+		title: "Software Engineer at Beam Dental",
+		desc: "Developed single paged applications using React and Ruby on Rails. Go team Space Whales",
+		className: "md:col-span-2", // change to md:col-span-2
+		thumbnail: "/beambenefits_logo.jpeg",
+	},
+	{
+		id: 4,
+		title: "Frontend Developer at Wired Campaigns",
+		desc: "Developed and maintained user-facing features using modern frontend technologies.",
+		className: "md:col-span-2",
+		thumbnail: "/exp4.svg",
+	},
 ];
 
 export const socialMedia = [
-    {
-        id: 1,
-        img: "/git.svg",
-        link: "https://github.com/JOATLGTM"
-    },
-    {
-        id: 3,
-        img: "/link.svg",
-        link: "https://www.linkedin.com/in/christophervoh/"
-    },
+	{
+		id: 1,
+		img: "/git.svg",
+		link: "https://github.com/JOATLGTM",
+	},
+	{
+		id: 3,
+		img: "/link.svg",
+		link: "https://www.linkedin.com/in/christophervoh/",
+	},
 ];
